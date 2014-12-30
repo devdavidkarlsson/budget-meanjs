6bdd024aea01fe2bd721cbea0a05f37bb87e7065
david <david@inkorgen.se>
2014-12-30 15:29:02 +0100

    Added a transactions menu in expense client config, since it is runned before configuring the incomes module, there should be a better place for this.

--- public/modules/expenses/config/expenses.client.config.js
+++ public/modules/expenses/config/expenses.client.config.js
@@ -4,8 +4,10 @@
 angular.module('expenses').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Expenses', 'expenses', 'dropdown', '/expenses(/create)?');
-		Menus.addSubMenuItem('topbar', 'expenses', 'List Expenses', 'expenses');
-		Menus.addSubMenuItem('topbar', 'expenses', 'New Expense', 'expenses/create');
+		//Menus.addMenuItem('topbar', 'Expenses', 'expenses', 'dropdown', '/expenses(/create)?');
+      //Where should i put this so it is guaranteed that it exists when all modules are configured
+      Menus.addMenuItem('topbar', 'Transactions', 'transactions', 'dropdown');
+      Menus.addSubMenuItem('topbar', 'transactions', 'List Expenses', 'expenses');
+	  Menus.addSubMenuItem('topbar', 'transactions', 'New Expense', 'expenses/create');
 	}
 ]);
